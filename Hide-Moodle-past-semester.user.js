6e8c02cde9a15137362c0f742f902f987488343d
micr0dust <wuilliamolgo104286@gmail.com>
2022-03-02 16:01:51 +0800

    commit

---
+++ Hide-Moodle-past-semester.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Hide-Moodle-past-semester
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  在暨大Moodle清單內移除舊學期課程
+// @author       Microdust
+// @match        https://moodle.ncnu.edu.tw/
+// @icon         https://moodle.ncnu.edu.tw/theme/image.php/lambda/theme/1646170574/favicon
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const date = new Date();
+    const thisSemester = (date.getFullYear() - 1912).toString() + semesterFn(date).toString();
+    const list = document.querySelector('#wrapper > header.navbar > nav > div > div > ul:nth-child(1) > li:nth-child(2) > ul');
+
+    for (let i = 0; i < list.childNodes.length; i++) {
+        let className = list.childNodes[i];
+        let semester = className.textContent.split('-')[0];
+        if (semester != thisSemester) list.childNodes[i].style.display = 'none';
+    }
+
+    function semesterFn(date) {
+        let month = date.getMonth() + 1;
+        if (month >= 2 && month <= 7) return 2;
+        return 1;
+    }
+})();
