5eb1b9c91d40ad5f13b35c84c381fb2b65bfe4fe
micr0dust <wuilliamolgo104286@gmail.com>
2022-03-04 18:47:04 +0800

    https://moodle.ncnu.edu.tw/*

--- Hide-Moodle-past-semester.user.js
+++ Hide-Moodle-past-semester.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  在暨大Moodle清單內移除舊學期課程
 // @author       Microdust
-// @match        https://moodle.ncnu.edu.tw/
+// @match        https://moodle.ncnu.edu.tw/*
 // @icon         https://moodle.ncnu.edu.tw/theme/image.php/lambda/theme/1646170574/favicon
 // @grant        none
 // ==/UserScript==
@@ -13,8 +13,10 @@
     'use strict';
     const date = new Date();
     const thisSemester = (date.getFullYear() - 1912).toString() + semesterFn(date).toString();
-    const list = document.querySelector('#wrapper > header.navbar > nav > div > div > ul:nth-child(1) > li:nth-child(2) > ul');
-
+    let path = document.querySelector('#wrapper > header.navbar > nav > div > div > ul:nth-child(1)');
+    if (location.href.split("?")[1]) path = document.querySelector('#custom_menu_activitystream');
+    path.querySelector('ul:nth-child(1) > li.dropdown.langmenu').style.display = 'none';
+    const list = path.querySelector('ul > li:nth-child(2) > ul');
     for (let i = 0; i < list.childNodes.length; i++) {
         let className = list.childNodes[i];
         let semester = className.textContent.split('-')[0];
